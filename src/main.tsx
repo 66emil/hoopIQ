87c7d33ad8ce16f7889be60ae15e1063f45dfc6f
66emil <emil.kabardaev@yandex.com>
2025-09-22 14:52:20 +0300

    Arc V

--- src/main.tsx
+++ src/main.tsx
@@ -1,12 +1,17 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import App from './App.tsx';
-import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import { createBrowserRouter, RouterProvider, useNavigate } from 'react-router-dom';
 import { Landing } from './components/Landing';
 import './index.css';
 
+function LandingRoute() {
+  const navigate = useNavigate();
+  return <Landing onStartLearning={() => navigate('/app/tactics')} />;
+}
+
 const router = createBrowserRouter([
-  { path: '/', element: <Landing onStartLearning={() => { window.location.href = '/app/tactics'; }} /> },
+  { path: '/', element: <LandingRoute /> },
   { path: '/app/*', element: <App /> }
 ]);
 
